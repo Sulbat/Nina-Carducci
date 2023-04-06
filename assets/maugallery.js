9de35a695ac0f120a2b806f37e2f383f3cf248dd
Sami Kibachi <samikibachi@gmail.com>
2023-04-06 02:09:36 +0200

    ajout images redimensionnées + rich snippets

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -13,6 +13,8 @@
       }
       $.fn.mauGallery.listeners(options);
 
+      debugger;
+
 
       $(this)
         .children(".gallery-item")
@@ -151,7 +153,7 @@
 
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i;
+          index = i-1;
         }
       });
       next =
@@ -190,7 +192,7 @@
 
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i;
+          index = i+1;
         }
       });
       next = imagesCollection[index] || imagesCollection[0];
